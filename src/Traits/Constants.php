8f6598f224ae7a166703ad281af7151e2d2e7bc7
Richard Banks <richard.banks@tradesafe.co.za>
2021-02-10 08:43:31 +0200

    chore: added enum lookup

--- src/Traits/Constants.php
+++ src/Traits/Constants.php
@@ -8,6 +8,23 @@ use GraphQL\Query;
 
 trait Constants
 {
+    public function getEnums(string $name)
+    {
+        $gql = (new Query('__type(name: "' . $name . '")'));
+
+        $gql->setSelectionSet([
+            (new Query('enumValues'))
+                ->setSelectionSet([
+                    'name',
+                    'description'
+                ])
+        ]);
+
+        $gqlResponse = self::callApi($gql, [], false);
+
+        return $gqlResponse['__type']['enumValues'];
+    }
+
     public function getIndustries()
     {
         $gql = (new Query('__type(name: "Industry")'));
@@ -28,6 +45,23 @@ trait Constants
     public function getTransactionStates()
     {
         $gql = (new Query('__type(name: "State")'));
+
+        $gql->setSelectionSet([
+            (new Query('enumValues'))
+                ->setSelectionSet([
+                    'name',
+                    'description'
+                ])
+        ]);
+
+        $gqlResponse = self::callApi($gql, [], false);
+
+        return $gqlResponse['__type']['enumValues'];
+    }
+
+    public function getFeeAllocations()
+    {
+        $gql = (new Query('__type(name: "FeeAllocation")'));
 
         $gql->setSelectionSet([
             (new Query('enumValues'))
